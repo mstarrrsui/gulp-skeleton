79794756e90abfa335ae54aa5aa7cdc616cd461f
Mike Starr <mstarr@rsui.com>
2016-04-08 16:06:53 -0400

    updates to jscs and jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,24 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
-var gulpprint = require('gulp-print');
+var gulpprint= require('gulp-print');
 
 gulp.task('vet', function() {
     return gulp
         .src([
-            './src/**/*.js',
+            //'./src/**/*.js',
             './*.js'
         ])
         .pipe(gulpprint())
         .pipe(jscs())
+        .pipe(jscs.reporter())
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish', {verbose: true}));
 });
+
+function foo() {
+    y = 2;
+    var x= 1
+}
+
+    
