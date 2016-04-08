e7fa33fa4127523bcbced93a5681c295c7626869
Mike Starr <mstarr@rsui.com>
2016-04-08 15:32:41 -0400

    renamed jshint and jscs files

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var gulpprint = require('gulp-print');
 
 gulp.task('vet', function() {
-    gulp.src([
-        './src/**/*.js',
-        './*.js'
-    ])
+    return gulp
+        .src([
+            './src/**/*.js',
+            './*.js'
+        ])
+        .pipe(gulpprint())
+        .pipe(jscs())
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish', {verbose: true}));
 });
