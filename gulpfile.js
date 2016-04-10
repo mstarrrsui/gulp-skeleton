15d04c15682a8088233a5ca3d6d43d258c511f4b
Michael Starr <mstarr@rsui.com>
2016-04-10 11:24:14 -0400

    added nodemon and browsersync tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 var gulp = require('gulp');
 var args = require('yargs').argv;
-//var browserSync = require('browser-sync');
+var browserSync = require('browser-sync');
 var config = require('./gulp.config')();
 //var del = require('del');
 //var path = require('path');
 //var _ = require('lodash');
 var $ = require('gulp-load-plugins')({lazy: true});
+var port = process.env.PORT || config.defaultPort;
 
 gulp.task('vet', function() {
 
@@ -41,6 +42,44 @@ gulp.task('inject', ['wiredep'], function() {
         .pipe(gulp.dest(config.client));
 });
 
+gulp.task('serve-dev', ['inject'], function() {
+
+    var isDev = true;
+    var nodeEnv = isDev ? 'dev' : 'build';
+
+    log('Serving up site in ' + nodeEnv + ' mode....');
+
+    var nodeOptions = {
+        script: config.nodeServer,
+        delayTime: 1,
+        env: {
+            'PORT': port,
+            'NODE_ENV': nodeEnv
+        },
+        watch: [config.server]
+    };
+
+    return $.nodemon(nodeOptions)
+        .on('restart', function(ev) {
+            log('*** nodemon restarted');
+            log('files changed on restart:\n' + ev);
+            // setTimeout(function() {
+            //     browserSync.notify('reloading now ...');
+            //     browserSync.reload({stream: false});
+            // }, config.browserReloadDelay);
+        })
+        .on('start', function() {
+            log('*** nodemon started');
+            startBrowserSync();
+        })
+        .on('crash', function() {
+            log('*** nodemon crashed: script crashed for some reason');
+        })
+        .on('exit', function() {
+            log('*** nodemon exited cleanly');
+        });
+});
+
 /////////////////////
 
 function log(msg) {
@@ -53,4 +92,49 @@ function log(msg) {
     } else {
         $.util.log($.util.colors.blue(msg));
     }
+}
+
+function startBrowserSync(isDev, specRunner) {
+    if (args.nosync || browserSync.active) {
+        return;
+    }
+
+    log('Starting browser-sync on port ' + port);
+
+    // if (isDev) {
+    //     gulp.watch([config.less], ['styles'])
+    //         .on('change', changeEvent);
+    // } else {
+    //     gulp.watch([config.less, config.js, config.html], ['optimize', browserSync.reload])
+    //         .on('change', changeEvent);
+    // }
+
+    var options = {
+        proxy: 'localhost:' + port,
+        port: 3000,
+        // files: isDev ? [
+        //     config.client + '**/*.*',
+        //     '!' + config.less,
+        //     config.temp + '**/*.css'
+        // ] : [],
+        files: [config.client + '**/*.*'],
+        ghostMode: {
+            clicks: true,
+            location: false,
+            forms: true,
+            scroll: true
+        },
+        injectChanges: true,
+        logFileChanges: true,
+        logLevel: 'debug',
+        logPrefix: 'gulp-skeleton',
+        notify: true,
+        reloadDelay: 0 //1000
+    };
+
+    // if (specRunner) {
+    //     options.startPath = config.specRunnerFile;
+    // }
+
+    browserSync(options);
 }
