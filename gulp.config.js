bf6ffd696191bfe806ba26259f9a7371b3df5215
Michael Starr <mstarr@rsui.com>
2016-04-09 22:18:27 -0400

    added tasks for injecting bower js and css

---
+++ gulp.config.js
@@ -0,0 +1,86 @@
+module.exports = function() {
+    var client = './src/client/';
+    var clientApp = client + 'app/';
+    var report = './report/';
+    var root = './';
+    var server = './src/server/';
+    var specRunnerFile = 'specs.html';
+    var temp = './.tmp/';
+    var wiredep = require('wiredep');
+    //var bowerFiles = wiredep({devDependencies: true})['js'];
+
+    var config = {
+        /**
+         * Files paths
+         */
+        alljs: [
+            './src/**/*.js',
+            './*.js'
+        ],
+        build: './build/',
+        client: client,
+        css: temp + 'styles.css',
+        fonts: './bower_components/font-awesome/fonts/**/*.*',
+        html: clientApp + '**/*.html',
+        htmltemplates: clientApp + '**/*.html',
+        images: client + 'images/**/*.*',
+        index: client + 'index.html',
+        js: [
+            clientApp + '**/*.module.js',
+            clientApp + '**/*.js',
+            '!' + clientApp + '**/*.spec.js'
+        ],
+        less: client + 'styles/styles.less',
+        report: report,
+        root: root,
+        server: server,
+        temp: temp,
+
+        /**
+         * optimized files
+         */
+        optimized: {
+            app: 'app.js',
+            lib: 'lib.js'
+        },
+
+        /**
+         * browser sync
+         */
+        browserReloadDelay: 1000,
+
+        /**
+         * Bower and NPM locations
+         */
+        bower: {
+            json: require('./bower.json'),
+            directory: './bower_components/',
+            ignorePath: '../..'
+        },
+        packages : [
+            './package.json',
+            './bower.json'
+        ],
+
+        /**
+         * Node settings
+         */
+        defaultPort: 7203,
+        nodeServer: './src/server/app.js'
+
+    };
+
+    config.getWiredepDefaultOptions = function() {
+        var options = {
+            bowerJson: config.bower.json,
+            directory: config.bower.directory,
+            ignorePath: config.bower.ignorePath
+        };
+        return options;
+    };
+
+    return config;
+
+    ////////////////
+
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,56 @@
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var jscs = require('gulp-jscs');
-var gulpprint= require('gulp-print');
+var args = require('yargs').argv;
+//var browserSync = require('browser-sync');
+var config = require('./gulp.config')();
+//var del = require('del');
+//var path = require('path');
+//var _ = require('lodash');
+var $ = require('gulp-load-plugins')({lazy: true});
 
 gulp.task('vet', function() {
+
+    log('Analyzing source with JSHint and JSCS');
+
     return gulp
-        .src([
-            //'./src/**/*.js',
-            './*.js'
-        ])
-        .pipe(gulpprint())
-        .pipe(jscs())
-        .pipe(jscs.reporter())
-        .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish', {verbose: true}));
+        .src(config.alljs)
+        .pipe($.if(args.verbose, $.print()))
+        .pipe($.jscs())
+        .pipe($.jscs.reporter())
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish', {verbose: true}));
 });
 
-function foo() {
-    y = 2;
-    var x= 1
-}
+gulp.task('wiredep', function() {
+    log('Wire up the bower css js and our app js into the html');
+    var options = config.getWiredepDefaultOptions();
+    var wiredep = require('wiredep').stream;
 
-    
+    return gulp
+        .src(config.index)
+        .pipe(wiredep(options))
+        .pipe($.inject(gulp.src(config.js)))
+        .pipe(gulp.dest(config.client));
+});
+
+gulp.task('inject', ['wiredep'], function() {
+    log('Wire up the app css into the html, and call wiredep ');
+
+    return gulp
+        .src(config.index)
+        .pipe($.inject(gulp.src(config.css)))
+        .pipe(gulp.dest(config.client));
+});
+
+/////////////////////
+
+function log(msg) {
+    if (typeof(msg) === 'object') {
+        for (var item in msg) {
+            if (msg.hasOwnProperty(item)) {
+                $.util.log($.util.colors.blue(msg[item]));
+            }
+        }
+    } else {
+        $.util.log($.util.colors.blue(msg));
+    }
+}
