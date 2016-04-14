566d7e9804e7f550448d162ab41f0e5969fec610
Mike Starr <mstarr@rsui.com>
2016-04-14 07:57:10 -0400

    udpates

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ gulp.task('vet', function() {
         .src(config.alljs)
         .pipe($.if(args.verbose, $.print()))
         .pipe($.jscs())
-        .pipe($.jscs.reporter())
+        //.pipe($.jscs.reporter())
         .pipe($.jshint())
         .pipe($.jshint.reporter('jshint-stylish', {verbose: true}));
 });
